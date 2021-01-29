a51df224accfb3d7d7bdbe0a4075bac53cf71d7d
tafseer-khan <tafseer.k.khan@gmail.com>
2021-01-29 13:51:42 -0600

    Added post function where information is required and max reservations is 5

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT|| 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Our data for reservations and waitlist
+
 const data = [
     {
         reservations: [],
@@ -18,6 +20,7 @@ const data = [
     }
 ]
 
+// Express routing for our pages and api
 app.get("/", function(req,res){
     res.sendFile(path.join(_dirname,"view.html"));
 })
@@ -31,6 +34,29 @@ app.get("/api/waitlist",function(req,res){
 app.get("/api/tables",function(req,res){
     res.json(data.reservations)
 })
+
+
+// Posts new reservation requests to /api/new
+// tableData must exist as well as the name of the reservation
+// If less than 5 reservations currently exists then tableData is then pushed to reservations
+// If more than 5 reservations currently exists then tableData is then pushed to waitlist 
+app.post("/api/new", function(req,res){
+    var tableData = req.body;
+    console.log(tableData)
+    if (tableData && tableData.name){
+        tableData.routeName = tableData.name
+    }
+    console.log(tableData)
+    if (data.reservations.length < 5){
+        data.reservations.push(tableData);
+    }
+    else{
+        data.waitlist.push(tableData)
+    }
+})
+
+
+
 
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT)
