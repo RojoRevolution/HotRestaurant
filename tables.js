7f6a7a4c03b2e7682f7fb9efe05e653980defffa
David Rojo <rojo@rojorevolution.com>
2021-01-29 14:14:44 -0600

    added a placeholder fetch functiont hat grabs table data

--- tables.js
+++ tables.js
@@ -1,2 +1,17 @@
 const tableUL = document.getElementById('tablelist')
 const waitListUL = document.getElementById('waitlist')
+
+
+// Fetch that GET's table info - placeholder code 
+fetch('NEED PATH', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+
+    })
+})
